5b3f5773c77c469feebec839fde5331c214cd51f
Amir Shabani <amishbni@gmail.com>
2024-12-10 21:57:09 +0330

    enable autosuggestion

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -79,6 +79,11 @@
     enable = true;
     oh-my-zsh.enable = true;
     enableCompletion = true;
+
+    autosuggestion = {
+      enable = true;
+    };
+
     syntaxHighlighting = {
       enable = true;
     };
