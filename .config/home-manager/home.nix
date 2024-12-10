2aa99278cdd9b54fe551325034c172e798253ceb
Amir Shabani <amishbni@gmail.com>
2024-12-10 22:39:52 +0330

    powerlevel10k

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -100,5 +100,11 @@
     shellAliases = {
       update = "sudo nixos-rebuild switch";
     };
+
+    initExtra = ''
+      # Powerlevel10k Zsh theme  
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
+      test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh  
+    '';
   };
 }
