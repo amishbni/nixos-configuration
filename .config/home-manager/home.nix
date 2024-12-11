50e5a997a7217cf79719ce2d97adc0de2f65a3f6
Amir Shabani <amishbni@gmail.com>
2024-12-11 13:27:53 +0330

    separate zsh config into its own file

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  # imports
+  imports = [
+    ./zsh.nix
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ami";
@@ -73,38 +77,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # ZSH
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-
-    oh-my-zsh = {
-      enable = true;
-    };
-
-    autosuggestion = {
-      enable = true;
-    };
-
-    syntaxHighlighting = {
-      enable = true;
-    };
-
-    history = {
-      size = 1000000;
-      save = 1000000;
-      path = "$HOME/.zsh_history";
-    };
-
-    shellAliases = {
-      update = "sudo nixos-rebuild switch";
-    };
-
-    initExtra = ''
-      # Powerlevel10k Zsh theme  
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
-      test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh  
-    '';
-  };
 }
