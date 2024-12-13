03bd5a04d7b395c22ab80a139be17939bf115874
Amir Shabani <amishbni@gmail.com>
2024-12-13 21:27:22 +0330

    dark theme with dark uri

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -79,7 +79,10 @@
   dconf.settings = {
     "org/gnome/desktop/background" = {
       picture-uri = "file:///home/ami/.config/wallpapers/rick.png";
-      picture-options = "stretched";
+      picture-uri-dark = "file:///home/ami/.config/wallpapers/rick.png";
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
     };
   };
 
