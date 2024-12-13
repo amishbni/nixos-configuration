c58c991d43dc691a4790e8a2506296f7c30f3fc1
Amir Shabani <amishbni@gmail.com>
2024-12-13 20:37:17 +0330

    dark theme for gnome

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -76,6 +76,12 @@
     GIT_EDITOR = "nvim";
   };
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
